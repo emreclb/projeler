fc09b84b1cb299a2744a69d2c105e63f2958093a
emreclb <emre04celebi@gmail.com>
2025-10-02 11:48:20 +0300

    mousemove

---
+++ mousemove/mousemove/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace mousemove
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragButtonPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // button1 olaylarını bağla
+            button1.MouseDown += Button1_MouseDown;
+            button1.MouseMove += Button1_MouseMove;
+            button1.MouseUp += Button1_MouseUp;
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragButtonPoint = button1.Location;
+            }
+        }
+
+        private void Button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point diff = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                button1.Location = Point.Add(dragButtonPoint, new Size(diff));
+            }
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = false;
+            }
+        }
+    }
+}
